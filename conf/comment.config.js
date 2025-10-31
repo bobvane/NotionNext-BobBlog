f20a8696e8ee5ba6e2f31cf02f0e67582ceb1628
bobvane <58875973+bobvane@users.noreply.github.com>
2025-10-31 12:14:32 +0800

    Fix typo in COMMENT_GITALK_OWNER variable

--- conf/comment.config.js
+++ conf/comment.config.js
@@ -54,7 +54,7 @@ module.exports = {
 
   // gitalk评论插件 更多参考 https://gitalk.github.io/
   COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || 'NotionNext-BobBlog', // 你的Github仓库名，例如 'NotionNext'
-  COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || bobvane'', // 你的用户名 e.g tangly1024
+  COMMENT_GITALK_OWNER: process.env.NEXT_PUBLIC_COMMENT_GITALK_OWNER || 'bobvane', // 你的用户名 e.g tangly1024
   COMMENT_GITALK_ADMIN: process.env.NEXT_PUBLIC_COMMENT_GITALK_ADMIN || 'bobvane', // 管理员用户名、一般是自己 e.g 'tangly1024'
   COMMENT_GITALK_CLIENT_ID:
     process.env.NEXT_PUBLIC_COMMENT_GITALK_CLIENT_ID || '', // e.g 20位ID ， 在gitalk后台获取
