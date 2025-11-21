c4840c57e9ef1b39c3de24a1b0580cff8c56d19c
bobvane <58875973+bobvane@users.noreply.github.com>
2025-11-22 00:05:26 +0800

    Update SideAreaRight.js

--- themes/next/components/SideAreaRight.js
+++ themes/next/components/SideAreaRight.js
@@ -35,8 +35,8 @@ const SideAreaRight = (props) => {
                 className='adsbygoogle'
                 style={{ display: 'block' }}
                 data-adtest='on'
-                data-ad-client='ca-pub-2708419466378217'
-                data-ad-slot='8807314373'
+                data-ad-client='ca-pub-1961997939597365'
+                data-ad-slot='2826775248'
                 data-ad-format='auto'
                 data-full-width-responsive='true'
             />
