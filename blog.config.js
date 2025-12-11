bf0cb86c841005b400015d75fac2e278e03f7c8d
bobvane <58875973+bobvane@users.noreply.github.com>
2025-12-11 14:39:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,7 @@ const BLOG = {
   API_BASE_URL: process.env.API_BASE_URL || 'https://bobvane.notion.site/api/v3', // API默认请求地址,可以配置成自己的地址例如：https://[xxxxx].notion.site/api/v3
   // Important page_id！！！Duplicate Template from  https://bobvane.notion.site/29a8421290f681d7acaee2f7858616cf
   NOTION_PAGE_ID:
-    process.env.NOTION_PAGE_ID ||
-    '29a8421290f681d7acaee2f7858616cf',
+    process.env.NOTION_PAGE_ID || '29a8421290f681d7acaee2f7858616cf',
   THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
